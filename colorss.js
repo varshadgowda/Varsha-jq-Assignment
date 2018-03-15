08f69d3d2714befe3a0afcdd3d66b4f0d579b753
varshadgowda <vdevendra@qwinix.io>
2018-03-15 17:16:13 +0530

    jquerytask

---
+++ colorss.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $("#demo").click(function(){
+    $("#demo").toggleClass("red");
+  });
+});
+
+$(document).ready(function(){
+  $("#demo1").click(function(){
+    $("#demo1").toggleClass("green");
+  });
+});
+
+
+$(document).ready(function(){
+  $("#demo2").click(function(){
+    $("#demo2").toggleClass("yellow");
+  });
+});
+
+
+
